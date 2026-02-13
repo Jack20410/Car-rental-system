d846a4f7e6f737ab2d907fde9a35ac273dcbdaa3
JackBao <522k0003@student.tdtu.edu.vn>
2026-02-13 12:03:29 -0600

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://car-rental-api-gateway.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
